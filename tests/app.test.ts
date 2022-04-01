8c0a4a159557ee013e8b710fa139a6c761e1f6b8
Simon Reekie <simon.reekie@cazoo.co.uk>
2022-04-01 15:11:01 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -4,9 +4,11 @@ type Inputer = (action: string) => void;
 
 class App {
   // eslint-disable-next-line no-empty-function
-  constructor(private _printer: Printer) {
+  constructor(private _printer: Printer, inputer: Inputer) {
     _printer("Lost in Cazoo Churchway");
     _printer("Cazoo Churchway description");
+    _printer("New Location Title");
+    _printer("New Location Description");
   }
 }
 
@@ -14,7 +16,7 @@ describe("App", () => {
   const mockPrinter = jest.fn();
 
   test("should show the title and description", () => {
-    const app = new App(mockPrinter);
+    const app = new App(mockPrinter, jest.fn());
     expect(mockPrinter).toHaveBeenNthCalledWith(1, "Lost in Cazoo Churchway");
     expect(mockPrinter).toHaveBeenNthCalledWith(
       2,
@@ -22,12 +24,21 @@ describe("App", () => {
     );
   });
 
-  test("should go north", () => {
-    const inputer = jest.fn();
-    const app = new App(mockPrinter, inputer);
-    inputer("GO N");
+  test("should go north and describe new location", () => {
+    const mockInputer = jest.fn();
+    const app = new App(mockPrinter, mockInputer);
+
+    mockInputer("GO N");
 
     expect(mockPrinter).toBeCalledWith("New Location Title");
     expect(mockPrinter).toBeCalledWith("New Location Description");
+  });
+
+  test("don't output location and description for no action", () => {
+    const mockInputer = jest.fn();
+    const app = new App(mockPrinter, mockInputer);
+
+    expect(mockPrinter).not.toBeCalledWith("New Location Title");
+    expect(mockPrinter).not.toBeCalledWith("New Location Description");
   });
 });
