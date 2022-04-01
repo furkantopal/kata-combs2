22a8c690035e5e36fd23ad1c58edbc9182135998
furkantopal <topallfurkan@gmail.com>
2022-04-01 16:01:59 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,21 +1,33 @@
 /* eslint-disable */
 type Printer = (line: string) => void;
+type Inputer = (action: string) => void;
 
 class App {
   // eslint-disable-next-line no-empty-function
-  constructor(private _printer: Printer) {}
+  constructor(private _printer: Printer) {
+    _printer("Lost in Cazoo Churchway");
+    _printer("Cazoo Churchway description");
+  }
 }
 
 describe("App", () => {
+  const mockPrinter = jest.fn();
+
   test("should show the title and description", () => {
-    const mockPrinter = jest.fn();
-
     const app = new App(mockPrinter);
-
     expect(mockPrinter).toHaveBeenNthCalledWith(1, "Lost in Cazoo Churchway");
     expect(mockPrinter).toHaveBeenNthCalledWith(
       2,
       "Cazoo Churchway description"
     );
+  });
+
+  test("should go north", () => {
+    const inputer = jest.fn();
+    const app = new App(mockPrinter, inputer);
+    inputer("GO N");
+
+    expect(mockPrinter).toBeCalledWith("New Location Title");
+    expect(mockPrinter).toBeCalledWith("New Location Description");
   });
 });
